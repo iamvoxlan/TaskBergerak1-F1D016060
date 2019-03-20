5b8b0f76143dda72c2078810538374b992f01362
iamvoxlan <iamvoxlan@github.com>
2019-03-20 13:26:32 +0800

    edit mainactivity

--- app/src/main/java/com/example/taskbergerak1f1d016060/MainActivity.java
+++ app/src/main/java/com/example/taskbergerak1f1d016060/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.taskbergerak1f1d016060;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.nio.file.Files;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         generateQuestion();
     }
 
@@ -23,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
     {
         Random rand = new Random();
         String question;
-        int randA = rand.nextInt(10)+1;
-        int randB = rand.nextInt(10)+1;
+        int randA = rand.nextInt(50)+1;
+        int randB = rand.nextInt(50)+1;
         int randOpt = rand.nextInt(3)+1;
 
         switch (randOpt)
@@ -45,15 +50,34 @@ public class MainActivity extends AppCompatActivity {
     }
     public void checkAnswer(View view) {
         EditText answerBox = findViewById(R.id.answerBox);
-        if (String)
-        if (userAnswer==answer)
+        String jawaban = answerBox.getText().toString();
+        if(jawaban.matches(""))
         {
-            TextView printQuestion = findViewById(R.id.questionText);
-            printQuestion.setText("Jawaban anda benar!");
+            alert();
+            generateQuestion();
         }
         else
         {
-            generateQuestion();
+            TextView peringatan = findViewById(R.id.commandText);
+            peringatan.setText("Masukkan hasil perhitungan di bawah dengan benar (bulatkan ke bawah)");
+            int userAnswer = Integer.valueOf(answerBox.getText().toString());
+            if (userAnswer==answer)
+            {
+                TextView printQuestion = findViewById(R.id.questionText);
+                printQuestion.setText("Jawaban anda benar!");
+                TextView resetButton = findViewById(R.id.submitButton);
+                resetButton.setText("Coba lagi");
+            }
+            else
+            {
+                generateQuestion();
+            }
         }
+    }
+
+    public void alert()
+    {
+        TextView peringatan = findViewById(R.id.commandText);
+        peringatan.setText("Masukkan jawaban!");
     }
 }
